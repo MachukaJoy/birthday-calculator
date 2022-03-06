3ae3b53bab5baf3766f8a7b410c47af0bff9f5b3
Machuka Joy <machukajoy@gmail.com>
2022-03-06 14:08:01 +0300

    Update javascript spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,27 @@
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-function validateFunction(){
+function validateFunction() {
   var akan_name = ""
 
   var gender = document.forms["userinput"]["gender"].value;
 
   var date = document.forms["userinput"]["date"].value;
 
-  var confirmDate = new Date(date); 
+  var confirmDate = new Date(date);
 
   var confirmDay = confirmDate.getDay();
 
-  if (gender === "Male"){
+  if (gender === "Male") {
     akan_name = maleNames[confirmDay];
   }
-  else if (gender === "Female"){
+  else if (gender === "Female") {
     akan_name = femaleNames[confirmDay]
-  } 
+  }
   else {
-    alert ("Date Invalid, Enter correct input");
+    alert("Date Invalid, Enter correct input");
   }
 
-  alert("Your akan name is " +akan_name);
+  alert("Your akan name is " + akan_name);
 
 }
