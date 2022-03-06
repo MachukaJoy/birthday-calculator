f9234680ab13fcdaaae8aa36418ad5bbe3e0ef7f
Machuka Joy <machukajoy@gmail.com>
2022-03-06 13:27:35 +0300

    Add variables to hold gender and date

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,15 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 function validateFunction(){
   var akan_name = ""
 
+  var gender = document.forms["userinput"]["gender"].value;
+
+  var date = document.forms["userinput"]["date"].value;
+  // change date format to be able to get day
+  
+  var confirmDate = new Date(date); 
+
+  var confirmDay = confirmDate.getDay();
+
 
 
 }
