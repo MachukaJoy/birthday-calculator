5b4fd8e486ef3829728895daad819663c29a63d2
Machuka Joy <machukajoy@gmail.com>
2022-03-06 13:18:55 +0300

    Create js function with return variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function validateFunction(){
+  var akan_name = ""
+
+
+
+}
+alert("Your akan name is " +akan_name);
